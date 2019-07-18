296ff306731af59b5641b5150ac281ae1796a1bd
nathan.kong <nathan.kong@trumptech.com>
2019-07-18 16:47:19 +0800

    Added UtilityBtnStack.swift

---
+++ pipiatum_partc/Components/MC/UtilityBtnStack.swift
@@ -0,0 +1,21 @@
+//
+//  UtilityBtnStack.swift
+//  pipiatum_partc
+//
+//  Created by Nathan Kong on 15/7/2019.
+//  Copyright © 2019 Nathan Kong. All rights reserved.
+//
+
+import UIKit
+
+class UtilityBtnStack: UIStackView {
+    
+    override required init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+}
